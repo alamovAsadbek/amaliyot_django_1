d6a74c696a6a9a7248a9b8a5cd0cce09326a9962
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-22 22:31:46 +0500

    change config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # home page url
+    path('', include('home.urls')),
+
 ]
